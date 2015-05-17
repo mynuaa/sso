e5265aa2a9fa62689ed46015ffc31b5d710bf33b
Rex Zeng <rex.fgtsky@qq.com>
2015-05-17 17:39:29 +0800

    修改三种登录顺序

--- page/login.php
+++ page/login.php
@@ -37,29 +37,12 @@ $queryCode = sha1(rand(10000) . "\t" . time());
 			<input type="button" onclick="window.location.href='?action=logout'" value="退出登录">
 		<?php else : ?>
 			<div class="tabs v3">
-				<div id="tab1" class="tab tab-current">论坛账号</div>
-				<div id="tab2" class="tab">学号</div>
-				<!-- <div id="tab3" class="tab">微信号</div> -->
+				<div id="tab1" class="tab tab-current">学号</div>
+				<div id="tab2" class="tab">论坛账号</div>
+				<div id="tab3" class="tab" style="color:#BBB">微信号</div>
 			</div>
 			<div class="groups">
 				<div id="group1" class="group group-current">
-					<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post" class="center" autocomplete="off">
-						<input type="hidden" name="token" value="<?php echo base64_encode(sha1(rand(10000))) ?>">
-						<input type="hidden" name="type" value="dz">
-						<div class="form-group">
-							<div><span class="field">账号</span></div>
-							<div><input type="text" name="username" class="area" required></div>
-						</div>
-						<div class="form-group">
-							<div><span class="field">密码</span></div>
-							<div><input type="password" name="password" class="area" required></div>
-						</div>
-					</form>
-					<div class="form-footer">
-						<input type="button" value="登录" onclick="document.querySelector('#group1>form').submit()">
-					</div>
-				</div>
-				<div id="group2" class="group">
 					<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post" class="center" autocomplete="off">
 						<input type="hidden" name="token" value="<?php echo base64_encode(sha1(rand(10000))) ?>">
 						<input type="hidden" name="type" value="ded">
@@ -76,12 +59,30 @@ $queryCode = sha1(rand(10000) . "\t" . time());
 						<input type="button" value="登录" onclick="document.querySelector('#group2>form').submit()">
 					</div>
 				</div>
-				<!-- <div id="group3" class="group">
-					<div>
-						<img id="wechat_qrcode" src="http://qr.liantu.com/api.php?text=<?php echo $queryCode; ?>" alt="扫码登录" style="width:200px;height:200px;border:2px solid;border-radius:0.5em;margin-bottom:0.5em">
-						<div id="wechat_tip" style="margin:0 1em;font-size:0.9em;text-align:left">* 请在公众号“飞机耳朵”的菜单中找到“纸飞机→万能扫码”，并将手机摄像头对准上方二维码。</div>
+				<div id="group2" class="group">
+					<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post" class="center" autocomplete="off">
+						<input type="hidden" name="token" value="<?php echo base64_encode(sha1(rand(10000))) ?>">
+						<input type="hidden" name="type" value="dz">
+						<div class="form-group">
+							<div><span class="field">账号</span></div>
+							<div><input type="text" name="username" class="area" required></div>
+						</div>
+						<div class="form-group">
+							<div><span class="field">密码</span></div>
+							<div><input type="password" name="password" class="area" required></div>
+						</div>
+					</form>
+					<div class="form-footer">
+						<input type="button" value="登录" onclick="document.querySelector('#group1>form').submit()">
 					</div>
-				</div> -->
+				</div>
+				<div id="group3" class="group">
+					<div>
+						<img id="wechat_qrcode" src="http://qr.liantu.com/api.php?text=即将推出，敬请期待<?php // echo $queryCode; ?>" alt="扫码登录" style="width:200px;height:200px;border:2px solid;border-radius:0.5em;margin-bottom:0.5em;opacity:0.3">
+						<!-- <div id="wechat_tip" style="margin:0 1em;font-size:0.9em;text-align:left">* 请在公众号“飞机耳朵”的菜单中找到“纸飞机→万能扫码”，并将手机摄像头对准上方二维码。</div> -->
+						<div id="wechat_tip" style="margin:0 1em;font-size:0.9em">* 微信登录功能即将推出，敬请期待！</div>
+					</div>
+				</div>
 			</div>
 		<?php endif; ?>
 		</div>
