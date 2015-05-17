9f1125e54d610c4b37fdd239b2f3d93d28a65db5
Rex Zeng <rex.fgtsky@qq.com>
2015-05-17 10:20:11 +0800

    修复登出时cookie未清空的BUG

--- code/logout.php
+++ code/logout.php
@@ -2,6 +2,7 @@
 
 if (isset($_SESSION['myauth_token'])) unset($_SESSION['myauth_token']);
 if (isset($_SESSION['myauth_uid'])) unset($_SESSION['myauth_uid']);
+setcookie('myauth_uid', '-1', time() - 1, '/');
 
 $url = isset($_GET['redirect_uri']) ? base64_decode($_GET['redirect_uri']) : '?page=login';
 
