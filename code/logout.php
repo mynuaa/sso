eb1dfdea8cd6443ce56ae427c714a9b7fce401d5
Rex Zeng <rex.fgtsky@qq.com>
2015-05-17 10:36:03 +0800

    .

--- code/logout.php
+++ code/logout.php
@@ -2,8 +2,7 @@
 
 if (isset($_SESSION['myauth_token'])) unset($_SESSION['myauth_token']);
 if (isset($_SESSION['myauth_uid'])) unset($_SESSION['myauth_uid']);
-setcookie('myauth_uid', '-1', time() - 1, '/');
-
+setcookie('myauth_uid', '', 0, '/');
 $url = isset($_GET['redirect_uri']) ? base64_decode($_GET['redirect_uri']) : '?page=login';
 
 header('Location: ' . $url);
