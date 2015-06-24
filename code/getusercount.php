bf6cff2de09fbdc5b8ee8fffca2374b39c62ea10
Rex Zeng <rex.fgtsky@qq.com>
2015-06-24 10:54:23 +0800

    禁止商家、新生、测试账号的小号

--- code/getusercount.php
+++ code/getusercount.php
@@ -6,7 +6,10 @@ $uid = uc_authcode($_COOKIE['myauth_uid'], 'DECODE', 'myauth');
 $uid = explode("\t", $uid)[1];
 
 $auth_ded = $db->result_first("SELECT `auth_ded` FROM `myauth` WHERE `auth_id` = $uid");
-if ($auth_ded == '000') die();
+
+// 禁止测试账号、新生、商家的小号
+if (in_array($auth_ded, array('JUST4TEST', 'FRESHMAN', 'MALLUSER'))) die();
+
 $result = $db->query("SELECT `auth_id` FROM `myauth` WHERE `auth_ded` = '$auth_ded'");
 
 $t = array();
