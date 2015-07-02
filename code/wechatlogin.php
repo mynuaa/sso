d39e7ea0b085659791c3816824e2cda9ec258904
Rex Zeng <rex.fgtsky@qq.com>
2015-07-02 20:14:59 +0800

    for debugging

--- code/wechatlogin.php
+++ code/wechatlogin.php
@@ -7,7 +7,7 @@ if ($param['action'] === 'set') {
 	(allAscii($queryCode[0]) && allAscii($queryCode[1]) && allAscii($queryCode[2])) || die();
 	echo $sql = "UPDATE `sso` SET `auth_logincode` = '{$queryCode[0]}' WHERE `auth_wechat` = '{$queryCode[2]}'";
 	$myauth->query($sql);
-	echo "\nQuery successful.";
+	echo "\n\nQuery successful. " . $myauth->affected_rows . " row(s) affected.";
 }
 else if ($param['action'] === 'get') {
 	$t = $myauth->result_first("SELECT `auth_id` FROM `sso` WHERE `auth_logincode` = '{$param['queryCode']}'");
