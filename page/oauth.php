c3d29de674f2eebb8ccbda98efd4d792fbf2278d
Rex Zeng <rex.fgtsky@qq.com>
2015-07-08 11:57:44 +0800

    添加判断应用是否被授权的代码

--- page/oauth.php
+++ page/oauth.php
@@ -4,9 +4,9 @@
 $appid = $_GET['appid'];
 $appsecret = $myauth->result_first("SELECT `appsecret` FROM `oauth_info` WHERE `appid` = '{$appid}'");
 $timestamp = $_GET['timestamp'];
-$validatecode = uc_authcode($appid . $appsecret . $timestamp, 'ENCODE', 'myauth');
+$validatecode = urlencode(uc_authcode($appid . $appsecret . $timestamp, 'ENCODE', 'myauth'));
 echo $appid, ' ', $appsecret, ' ', $timestamp, ' ', $validatecode, ' ', $_GET['authcode'];
-if ($validatecode !== urldecode($_GET['authcode'])) die('该应用未被授权！');
+if ($validatecode !== $_GET['authcode']) die('该应用未被授权！');
 
 if (isset($_GET['access_token'])) {
 	$result = array();
