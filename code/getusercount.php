c6b774b15defc19d830c111013be8c90f13e0920
Rex Zeng <rex.fgtsky@qq.com>
2015-08-05 01:55:13 +0800

    修复无法切换马甲的BUG

--- code/getusercount.php
+++ code/getusercount.php
@@ -2,8 +2,8 @@
 
 (!isset($_COOKIE['myauth_uid'])) && die();
 
-$uid = my_encrypt($_COOKIE['myauth_uid']);
-$uid = explode("\t", $uid)[1];
+$uid = json_decode(my_decrypt($_COOKIE['myauth_uid']), true);
+$uid = intval($uid['uid']);
 
 $auth_ded = $myauth->result_first("SELECT `auth_ded` FROM `sso` WHERE `auth_id` = $uid");
 
