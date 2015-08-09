12b72136b982a5a9df99712bbfbeddbff585cad2
Rex Zeng <rex.fgtsky@qq.com>
2015-08-09 17:46:22 +0800

    完善获取openid的页面

--- code/getopenid.php
+++ code/getopenid.php
@@ -1,5 +1,9 @@
 <?
 
-$code = file_get_contents('https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxa04c7656484a07d2&redirect_uri=http%3A%2F%2Fmy.nuaa.edu.cn%2Fsso%2F%3Faction%3Ddump&response_type=code&scope=snsapi_base&state=my.nuaa.edu.cn/sso/?action=dump#wechat_redirect');
-$code = preg_match('/code=.?&/', $code);
-var_dump($code);
+if (!isset($_GET['code']))
+	header("Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxa04c7656484a07d2&redirect_uri=http%3A%2F%2Fmy.nuaa.edu.cn%2Fsso%2F%3Faction%3Dgetopenid&response_type=code&scope=snsapi_base&state=mynuaa#wechat_redirect");
+else {
+	$result = file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxa04c7656484a07d2&secret=66fe85f09de7ce2fac6d11e075886686&code={$_GET['code']}&grant_type=authorization_code");
+	$result = json_decode($result);
+	var_dump($result);
+}
